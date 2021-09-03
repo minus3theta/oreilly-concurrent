39136bb893483c356d2a240eaad1b4ce4a7bd504
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-09-04 03:10:38 +0900

    Add barrier example (3.8.4)

---
+++ src/bin/c3_barrier.rs
@@ -0,0 +1,23 @@
+use std::{
+    sync::{Arc, Barrier},
+    thread,
+};
+
+fn main() {
+    let mut v = Vec::new();
+
+    let barrier = Arc::new(Barrier::new(10));
+
+    for _ in 0..10 {
+        let b = barrier.clone();
+        let th = thread::spawn(move || {
+            b.wait();
+            println!("finished barrier");
+        });
+        v.push(th);
+    }
+
+    for th in v {
+        th.join().unwrap();
+    }
+}
