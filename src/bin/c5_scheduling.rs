286285902aa7b7d2a4c6dd0e09b8ceda7bb8dda9
minus3theta <yone_j_synthesis@yahoo.co.jp>
2021-09-11 20:49:03 +0900

    Add c5_scheduling.rs (5.2.2)

---
+++ src/bin/c5_scheduling.rs
@@ -0,0 +1,118 @@
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::{
+        mpsc::{sync_channel, Receiver, SyncSender},
+        Arc, Mutex,
+    },
+    task::{Context, Poll},
+};
+
+use futures::{
+    future::{BoxFuture, FutureExt},
+    task::{waker_ref, ArcWake},
+};
+
+struct Task {
+    future: Mutex<BoxFuture<'static, ()>>,
+    sender: SyncSender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        let self0 = arc_self.clone();
+        arc_self.sender.send(self0).unwrap();
+    }
+}
+
+struct Executor {
+    sender: SyncSender<Arc<Task>>,
+    receiver: Receiver<Arc<Task>>,
+}
+
+impl Executor {
+    fn new() -> Self {
+        let (sender, receiver) = sync_channel(1024);
+        Executor {
+            sender: sender.clone(),
+            receiver,
+        }
+    }
+
+    fn get_spawner(&self) -> Spawner {
+        Spawner {
+            sender: self.sender.clone(),
+        }
+    }
+
+    fn run(&self) {
+        while let Ok(task) = self.receiver.recv() {
+            let mut future = task.future.lock().unwrap();
+            let waker = waker_ref(&task);
+            let mut ctx = Context::from_waker(&waker);
+            let _ = future.as_mut().poll(&mut ctx);
+        }
+    }
+}
+
+struct Spawner {
+    sender: SyncSender<Arc<Task>>,
+}
+
+impl Spawner {
+    fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
+        let future = future.boxed();
+        let task = Arc::new(Task {
+            future: Mutex::new(future),
+            sender: self.sender.clone(),
+        });
+
+        self.sender.send(task).unwrap();
+    }
+}
+
+struct Hello {
+    state: StateHello,
+}
+
+enum StateHello {
+    HELLO,
+    WORLD,
+    END,
+}
+
+impl Hello {
+    fn new() -> Self {
+        Hello {
+            state: StateHello::HELLO,
+        }
+    }
+}
+
+impl Future for Hello {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> std::task::Poll<Self::Output> {
+        match (*self).state {
+            StateHello::HELLO => {
+                print!("Hello, ");
+                (*self).state = StateHello::WORLD;
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            }
+            StateHello::WORLD => {
+                println!("World!");
+                (*self).state = StateHello::END;
+                cx.waker().wake_by_ref();
+                Poll::Pending
+            }
+            StateHello::END => Poll::Ready(()),
+        }
+    }
+}
+
+fn main() {
+    let executor = Executor::new();
+    executor.get_spawner().spawn(Hello::new());
+    executor.run();
+}
